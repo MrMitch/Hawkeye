1d4fce151008155599b3b5ed721c15db8067c7df
Mickael GOETZ <mickael.goetz@gmail.com>
2013-04-08 22:43:40 +0200

    move config to a single

--- config/__init__.py
+++
@@ -1,3 +0,0 @@
-from config import APP_NAME, CONSUMER_KEY, CONSUMER_SECRET, CONF, write_configuration_file, BASE
-
-__all__ = [APP_NAME, CONSUMER_KEY, CONSUMER_SECRET, CONF, write_configuration_file, BASE]

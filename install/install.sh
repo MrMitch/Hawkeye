aafad8528c1dce8eb57af241520123595a8d3e1d
Mickael GOETZ <mickael.goetz@gmail.com>
2013-04-13 00:46:28 +0200

    please SublimeText, stop using TABS INSTEAD OF FUCKING SPACES

--- install/install.sh
+++ install/install.sh
@@ -3,74 +3,74 @@
 ORIGIN_DIR=`pwd`
 
 if [[ `whoami` == 'root' ]]
-	then
-	TMP_DIR=/tmp/hawkeye
-	INSTALLATION_DIR=/etc/hawkeye
-	HAWKEYE_CONFIG=/usr/local/bin/hawkeye-config
+    then
+    TMP_DIR=/tmp/hawkeye
+    INSTALLATION_DIR=/etc/hawkeye
+    HAWKEYE_CONFIG=/usr/local/bin/hawkeye-config
 
-	echo "Creating hawkeye user and group..."
-	useradd -r -s /bin/false hawkeye
-	echo "...done"
-	echo ""
+    echo "Creating hawkeye user and group..."
+    useradd -r -s /bin/false hawkeye
+    echo "...done"
+    echo ""
 
-	echo "Fetching hawkeye's sources from GitHub..."
-	git clone https://github.com/MrMitch/Hawkeye.git $TMP_DIR
-	CLONE="$?"
+    echo "Fetching hawkeye's sources from GitHub..."
+    git clone https://github.com/MrMitch/Hawkeye.git $TMP_DIR
+    CLONE="$?"
 
-	if [[ $CLONE != 0 ]]
-		then
-		echo '...fail!'
-		exit
-	fi
+    if [[ $CLONE != 0 ]]
+        then
+        echo '...fail!'
+        exit
+    fi
 
-	echo "...done"
-	echo ""
+    echo "...done"
+    echo ""
 
-	echo "Fetching hawkeye's dependencies..."
-	cd $TMP_DIR
-	git submodule init
-	git submodule update
-	echo "...done"
-	echo ""
+    echo "Fetching hawkeye's dependencies..."
+    cd $TMP_DIR
+    git submodule init
+    git submodule update
+    echo "...done"
+    echo ""
 
-	mv /tmp/hawkeye/ $INSTALLATION_DIR
+    mv /tmp/hawkeye/ $INSTALLATION_DIR
 
-	echo "Creating hawkeye service launcher and config binary..."
-	ln -s $INSTALLATION_DIR/configure.py $HAWKEYE_CONFIG
-	ln -s $INSTALLATION_DIR/install/service_launcher.sh /etc/init.d/hawkeye
-	echo "...done"
-	echo ""
+    echo "Creating hawkeye service launcher and config binary..."
+    ln -s $INSTALLATION_DIR/configure.py $HAWKEYE_CONFIG
+    ln -s $INSTALLATION_DIR/install/service_launcher.sh /etc/init.d/hawkeye
+    echo "...done"
+    echo ""
 
-	# chown -R hawkeye:hawkeye /etc/hawkeye
+    # chown -R hawkeye:hawkeye /etc/hawkeye
 
-	echo -n "Should hawkeye start on boot ? [yes/no] "
-	read BOOT
+    echo -n "Should hawkeye start on boot ? [yes/no] "
+    read BOOT
 
-	if [[ $BOOT == "y" || $BOOT == "yes" ]]
-		then
-		echo "Configuring hawkeye to start on boot..."
-		update-rd.d hawkeye defaults
-		echo "...done"
-		echo ""
-	fi
+    if [[ $BOOT == "y" || $BOOT == "yes" ]]
+        then
+        echo "Configuring hawkeye to start on boot..."
+        update-rd.d hawkeye defaults
+        echo "...done"
+        echo ""
+    fi
 
-	echo "To control hawkeye manualy, you can run (as root): "
-	echo "service hawkeye {status|start|stop|restart}"
+    echo "To control hawkeye manualy, you can run (as root): "
+    echo "service hawkeye {status|start|stop|restart}"
 
-	echo "Cleaning installation files..."
-	rm -rf $TMP_DIR
-	cd $ORIGIN_DIR
-	echo "...done"
-	echo ""
+    echo "Cleaning installation files..."
+    rm -rf $TMP_DIR
+    cd $ORIGIN_DIR
+    echo "...done"
+    echo ""
 
-	echo "Hawkeye is now installed, BUT NOT CONFIGURED, You can configure hawkeye by running: hawkeye-config."
-	echo -n "Should this script invoke it for you now ? [yes/no]"
-	read CONFIG
+    echo "Hawkeye is now installed, BUT NOT CONFIGURED, You can configure hawkeye by running: hawkeye-config."
+    echo -n "Should this script invoke it for you now ? [yes/no]"
+    read CONFIG
 
-	if [[ $CONFIG == "y" || $CONFIG == "yes" ]]
-		then
-		python $HAWKEYE_CONFIG
-	fi
+    if [[ $CONFIG == "y" || $CONFIG == "yes" ]]
+        then
+        python $HAWKEYE_CONFIG
+    fi
 else
-	echo "This script must be run as root (or using sudo)"
+    echo "This script must be run as root (or using sudo)"
 fi
